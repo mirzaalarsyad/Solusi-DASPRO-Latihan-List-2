a6a2117ab8563ed8a9f7f6440782b5f46754143b
vivo2419 <47889304+vivo2419@users.noreply.github.com>
2019-02-27 22:18:11 +0700

    Add files via upload

---
+++ SplitAlternate.hs
@@ -0,0 +1,12 @@
+splitAlternate :: [Char]-> ([Char],[Char])
+
+splitAlternate l =
+            let odd l  |null l = []
+                       |length l ==1 = [head l]
+                       |otherwise= [head l] ++ odd (tail(tail l))
+                even l |null l = []
+                       |length l==1 = tail l
+                       |otherwise= [head (tail l)] ++ even (tail(tail l))
+            in
+                (odd l, even l)
+      
